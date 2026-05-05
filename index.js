66d09df70a441926caf04a98ad24656ce0110d75
saniya <saniyagarg9@gmail.com>
2026-05-05 19:12:19 +0530

    done:

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type"]
 }));
 
-app.options("*", cors());   // ✅ HANDLE PREFLIGHT EXPLICITLY
+app.use(cors());   // ✅ HANDLE PREFLIGHT EXPLICITLY
 
 app.use(express.json());
 
